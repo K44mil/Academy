cac42b0ec7c1e934e22e7dd29c0143ccc07ced0e
K44mil <kd155@o2.pl>
2020-09-02 01:23:00 -0700

    task_2 complete

--- app/code/Bulbulatory/Recomendations/registration.php
+++ app/code/Bulbulatory/Recomendations/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Snowdog_TestModule',
+    'Bulbulatory_Recomendations',
     __DIR__
 );
